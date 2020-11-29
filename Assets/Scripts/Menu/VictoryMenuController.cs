c23f9f975723cfbad9b1d2c770791877ad0e2f3f
Michael West <19785650+itsmichaelwest@users.noreply.github.com>
2020-11-29 20:26:33 +0000

    Updates

---
+++ Assets/Scripts/Menu/VictoryMenuController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class VictoryMenuController : MenuController
+{
+    //public string               NextLevel;
+    public GameObject           Menu;
+    private static GameObject   MenuStatic;
+    private static Button       NextLevelButton;
+    private static Button       RestartButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MenuStatic = Menu;
+        MenuStatic.SetActive(false);
+        NextLevelButton = transform.GetChild(1).GetComponent<Button>();
+        NextLevelButton.onClick.AddListener(LoadNextLevel);
+        RestartButton = transform.GetChild(2).GetComponent<Button>();
+        RestartButton.onClick.AddListener(RestartLevel);
+    }
+
+    public static void Show()
+    {
+        Time.timeScale = 0;
+        MenuStatic.SetActive(true);
+        gameStarted = false;
+    }
+
+
+    public static void LoadNextLevel()
+    {
+        SceneManager.LoadScene(VictoryManager._NextLevel);
+    }
+}
---
+++ Assets/Scripts/Player/VictoryManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class VictoryManager : MonoBehaviour
+{
+    public static string    _NextLevel;
+    public string           NextLevel;
+
+    private void Start()
+    {
+        _NextLevel = NextLevel;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            VictoryMenuController.Show();
+        }
+    }
+}
